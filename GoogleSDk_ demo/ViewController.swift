e0be6df97e2c47a857980dd6034ef9cdc7ef330e
Rajat Bhatt <rajatbhatt@ansh100.local>
2017-07-12 12:57:39 +0530

    Get address fields from places.addressComponents

--- GoogleSDk_ demo/ViewController.swift
+++ GoogleSDk_ demo/ViewController.swift
@@ -48,6 +48,29 @@ class ViewController: UIViewController {
             }
         })
     }
+    
+//    // Populate the address form fields.
+//    func fillAddressForm() {
+//        address_line_1.text = street_number + " " + route
+//        city.text = locality
+//        state.text = administrative_area_level_1
+//        if postal_code_suffix != "" {
+//            postal_code_field.text = postal_code + "-" + postal_code_suffix
+//        } else {
+//            postal_code_field.text = postal_code
+//        }
+//        country_field.text = country
+//
+//        // Clear values for next time.
+//        street_number = ""
+//        route = ""
+//        neighborhood = ""
+//        locality = ""
+//        administrative_area_level_1  = ""
+//        country = ""
+//        postal_code = ""
+//        postal_code_suffix = ""
+//    }
 }
 
 extension ViewController: GMSAutocompleteViewControllerDelegate {
@@ -76,6 +99,43 @@ extension ViewController: GMSAutocompleteViewControllerDelegate {
             print("Place placeID \(String(describing: place.placeID))")
             print("Place attributions \(String(describing: place.attributions))")
         })
+        
+        // Get the address components.
+        if let addressLines = place.addressComponents {
+            // Populate all of the address fields we can find.
+            for field in addressLines {
+                switch field.type {
+                case kGMSPlaceTypeStreetNumber:
+                    let street_number = field.name
+                    print(street_number)
+                case kGMSPlaceTypeRoute:
+                    let route = field.name
+                    print(route)
+                case kGMSPlaceTypeNeighborhood:
+                    let neighborhood = field.name
+                    print(neighborhood)
+                case kGMSPlaceTypeLocality:
+                    let locality = field.name
+                    print(locality)
+                case kGMSPlaceTypeAdministrativeAreaLevel1:
+                    let administrative_area_level_1 = field.name
+                    print(administrative_area_level_1)
+                case kGMSPlaceTypeCountry:
+                    let country = field.name
+                    print(country)
+                case kGMSPlaceTypePostalCode:
+                    let postal_code = field.name
+                    print(postal_code)
+                case kGMSPlaceTypePostalCodeSuffix:
+                    let postal_code_suffix = field.name
+                    print(postal_code_suffix)
+                // Print the items we aren't using.
+                default:
+                    print("Type: \(field.type), Name: \(field.name)")
+                }
+            }
+        }
+        
         dismiss(animated: true, completion: nil)
     }
     
